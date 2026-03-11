58785c2c020d3a62a3314eeb7611382afdb6ab5f
Werwllleh <allvxm4@gmail.com>
2026-03-11 21:53:57 +0100

    fix

--- src/main.ts
+++ src/main.ts
@@ -21,8 +21,8 @@ async function bootstrap() {
     credentials: true,
   });
 
-  const port = process.env.PORT ?? 3000;
+  const port = process.env.PORT ?? 3180;
   await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}`);
+  console.log(`Application is running on PORT: ${port}`);
 }
 bootstrap();
